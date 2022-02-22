752798cb20b7ee0ecdca6ee66bd6a3660c0a7a55
tun-ji <oluwatomilola.adeniran@pau.edu.ng>
2022-02-22 18:34:19 +0100

    Added support for template rendering

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
+import * as nunjucks from 'nunjucks'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const express = app.getHttpAdapter().getInstance();
+  const views = join(__dirname, '..', 'views');
+  const staticAssets = join(__dirname, '..', 'static');
+  app.useStaticAssets(staticAssets);
+  nunjucks.configure(views, {express})
   await app.listen(3000);
 }
 bootstrap();
